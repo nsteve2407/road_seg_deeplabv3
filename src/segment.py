a97ae5fddab56ca7dc7df53b682114f4b6ed9aeb
Stephen Ninan <nstephen@tamu.edu>
2021-11-30 20:34:40 -0600

    Time Stamp bug fix

--- src/segment.py
+++ src/segment.py
@@ -12,17 +12,31 @@ import cv2
 
 
 class Segmenter():
-    def __init__(self,path_to_weights):
+    def __init__(self,path_to_weights,publish_image=True):
         self.publisher = rospy.Publisher("road_points",PointCloud2,queue_size=100)
         self.num_classes = 1
         self.img_height = 128
         self.path = path_to_weights
         self.model = DeeplabV3Plus(self.img_height,self.num_classes)
         self.model.load_weights(self.path)
+        self.img_publisher = rospy.Publisher("road_segment_image",Image,queue_size=100)
+        self.publish_image = publish_image
 
-    def init_model(self):
-        self.model = DeeplabV3Plus(self.img_height,self.num_classes)
-        self.model.load_weights(self.path)
+    def decode_segmentation_masks(self,mask, colormap, n_classes):
+        
+        g = np.zeros_like(mask).astype(np.float32)
+        b = np.zeros_like(mask).astype(np.float32)
+        r = mask
+        rgb = np.stack([b, g, r],axis=-1)
+        rgb = np.reshape(rgb,(128,1024,3))
+        # print('Source mask shape:{}'.format(rgb.shape))
+        return rgb
+
+
+    def get_overlay(self,image, colored_mask):
+        np.reshape(colored_mask,(128,1024,3))
+        overlay = cv2.addWeighted(image, 0.8, colored_mask.astype(np.uint8), 70.0, 0)
+        return overlay
 
     def pc_cb(self,pc_data,image_msg):
         array = ros_numpy.point_cloud2.pointcloud2_to_array(pc_data)
@@ -42,14 +56,23 @@ class Segmenter():
         # Publish PCloud
 
         # Convert intensity values to 255 scale?
-        op = op*255
+        op_rs = op*255
 
         # Publish
         pcd = np.copy(array)
-        pcd['intensity'] = op.astype('<f4') # Need to change dtpye?
+        pcd['intensity'] = op_rs.astype('<f4') # Need to change dtpye?
         msg = ros_numpy.point_cloud2.array_to_pointcloud2(pcd)
-        msg.header.frame_id = pc_data.header.frame_id
+        msg.header = pc_data.header
         self.publisher.publish(msg)
+
+        if self.publish_image:
+            op = np.expand_dims(op,axis=-1)
+            rgb_mask = self.decode_segmentation_masks(op,[],self.num_classes)
+            segmented_image = self.get_overlay(img,rgb_mask)
+            segmented_image = bridge.cv2_to_imgmsg(segmented_image,"bgr8")
+            self.img_publisher.publish(segmented_image)
+
+
 
 
 def main():
